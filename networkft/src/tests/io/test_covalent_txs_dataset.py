84761ac5df0f2bc8b2eaf1cea3538f7c996e249c
craycrayfish <shawntan72@gmail.com>
2022-06-13 00:53:26 +0100

    Added notebooks to download raw data from covalent api, and custom datasets to load the downloaded json datasets

---
+++ networkft/src/tests/io/test_covalent_txs_dataset.py
@@ -0,0 +1,19 @@
+"""Unit test for CovalentDataSet"""
+
+import json
+from pathlib import Path
+
+from networkft.io.covalent_txs_dataset import CovalentDataSet
+
+
+TEST_TXS_DIRECTORY = str(
+    Path(__file__).resolve().parents[1] / "test_data" / "covalent_txs"
+)
+
+
+def test_covalent_txs_dataset():
+    """Test that CovalentDataSet is able to read nft_txs json files correctly"""
+    dataset = CovalentDataSet(TEST_TXS_DIRECTORY)
+    txs = dataset._load()
+    assert type(txs) is dict
+    assert list(txs["test"]["0"]) == ["updated_at", "txs"]
