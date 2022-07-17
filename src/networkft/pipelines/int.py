70a01066596c14eddda09a0446a2e7567ac7be84
craycrayfish <shawntan72@gmail.com>
2022-07-17 19:43:49 +0100

    Added type conversion to int pipeline

--- src/networkft/pipelines/int.py
+++ src/networkft/pipelines/int.py
@@ -4,6 +4,7 @@ Transforms raw data to intermediate layer
 
 from kedro.pipeline import Pipeline, node
 
+from networkft.nodes.column_typing import convert_timestamp, convert_wei
 from networkft.nodes.covalent_txs import parse_covalent_txs
 
 
@@ -13,12 +14,24 @@ def create_int_covalent_pipeline(**kwargs):
             node(
                 func=parse_covalent_txs,
                 inputs=[
-                    "raw_covalent_nft_txs",
+                    "raw_covalent_txs",
                     "params:covalent.tx_metadata",
                     "params:covalent.tx_params",
                 ],
-                outputs="int_covalent_nft_txs",
+                outputs="raw_covalent_txs_parsed",
                 name="int_parse_covalent_txs_node",
-            )
+            ),
+            node(
+                func=convert_timestamp,
+                inputs=["raw_covalent_txs_parsed", "params:covalent.convert_timestamp"],
+                outputs="raw_covalent_txs_parsed_ts",
+                name="int_convert_ts_covalent_txs_node",
+            ),
+            node(
+                func=convert_wei,
+                inputs=["raw_covalent_txs_parsed_ts", "params:covalent.convert_wei"],
+                outputs="int_covalent_txs",
+                name="int_convert_wei_covalent_txs_node",
+            ),
         ]
     )
