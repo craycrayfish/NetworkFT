87854d30c870969e80fdab39b8c306ccc6a635fe
craycrayfish <shawntan72@gmail.com>
2022-07-23 00:15:36 +0100

    unidirectional node

--- src/networkft/nodes/graph_generation.py
+++ src/networkft/nodes/graph_generation.py
@@ -1,23 +1,39 @@
 """Nodes to generate graph of capital flow between projects"""
-from typing import List
+from typing import Dict
 import pandas as pd
 
 
 def node_convert_to_unidirectional(
         _df: pd.DataFrame,
-        cols: List = ["from", "to"],
+        cols: Dict = {"from": "in", "to": "out"},
         entity_col: str = "entity"
 ):
     """Node to convert table of transactions from bidirectional to unidirectional
     format.
+    Initial: | from | to | value |  Final: | entity | value | direction |
+             | AA   | BB | 1.0   |         | AA     | 1.0   | in        |
+                                           | BB     | 1.0   | out       |
 
     Args:
-        _df:
-        cols:
-        entity_col:
+        _df: df representing transactions with columns for sending and receiving parties
+        cols: column labels for the sending and receiving addresses, and their
+        respective directions
+        entity_col: column label to be created containing the from and to addresses
 
     Returns:
 
     """
-    df = _df.copy()
-    return df
+    assert len(cols) == 2, "`cols` should contain only the from and to labels"
+
+    dfs = []
+    for col, direction in cols.items():
+        df = _df.copy()
+        df["direction"] = direction
+        df = df.rename(
+            columns={col: entity_col}
+        ).drop(
+            [c for c in cols if c != col], axis=1
+        )
+        dfs.append(df)
+
+    return pd.concat(dfs)
---
+++ src/tests/nodes/test_graph_generation.py
@@ -0,0 +1,7 @@
+"""Tests for graph generation nodes"""
+from pandas import pd
+
+from networkft.nodes.graph_generation import node_convert_to_unidirectional
+
+
+def test_node_convert_to_unidirectional():
